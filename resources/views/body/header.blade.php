c8065dd3cebfedfb87ad218ed442738c3bc893a4
ae <asoulipanya@gmail.com>
2021-08-05 16:54:37 +0700

    new web app

---
+++ resources/views/body/header.blade.php
@@ -0,0 +1,44 @@
+<!-- ======= Header ======= -->
+<header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+        <div class="logo mr-auto">
+            <a href="index.html"><img src="{{asset('frontend/assets/img/logo.png')}}" title="ກົມການຄ້າພາຍໃນ ກະຊວງອຸດສາຫະກໍາ ແລະ ການຄ້າ" alt="Department of Internal Trade" class="img-fluid"></a>ກົມການຄ້າພາຍໃນ
+        </div>
+        <nav class="nav-menu d-none d-lg-block">
+            <ul>
+                <li class="active"><a href="index.html">ໜ້າທໍາອິດ</a></li>
+                <li class="drop-down">
+                    <a href="#">ກ່ຽວກັບພວກເຮົາ</a>
+                    <ul>
+                        <li><a href="history.html">ປະຫວັດຄວາມເປັນມາ</a></li>
+                        <li><a href="structure.html">ໂຄງຮ່າງການຈັດຕັ້ງ</a></li>
+                        <!--                            <li><a href="human.html">ບຸກຄະລາກອນ</a></li> -->
+                        <li><a href="role.html">ພາລະບົດບາດ</a></li>
+                        <!--                            <li><a href="unit.html">ໜ່ວຍງານໃນສັງກັດ</a></li> -->
+                        <li><a href="white_report.html">ບົດລາຍງານປະຈຳປີ</a></li>
+                        <li><a href="strategy.html">ແຜນຍຸດທະສາດ ແລະ ວິໄສທັດ</a></li>
+                    </ul>
+                </li>
+                <li class="drop-down"><a href="#">ຂ່າວ/ປະກາດຂອງກົມ</a>
+                    <ul>
+                        <li><a href="fuel_price.html">ແຈ້ງການປັບລາຄານໍ້າມັນ</a></li>
+                        <li><a href="news.html">ຂ່າວປະຊາສຳພັນ</a></li>
+                        <li><a href="activity.html">ຮູບພາບກິດຈະກຳ</a></li>
+                    </ul>
+                </li>
+                <li><a href="statistic.html">ຂໍ້ມູນສະຖິຕິ/ບົດຄົ້ນຄວ້າ</a></li>
+                <li class="drop-down"><a href="#">ນິຕິກຳຂອງກົມ</a>
+                    <ul>
+                        <li><a href="legislation.html">ກົດໝາຍ ແລະ ດຳລັດ</a></li>
+                        <li><a href="order.html">ຄຳສັ່ງ ແລະ ຂໍ້ຕົກລົງ</a></li>
+                        <li><a href="notice.html">ບົດແນະນຳ ແລະ ແຈ້ງການ</a></li>
+                        <li><a href="https://law.moic.gov.la/lo/" target="_blank">ຖານຂໍ້ມູນນິຕິກຳ ອຄ</a></li>
+                    </ul>
+                </li>
+                <li><a href="contact.html">ຕິດຕໍ່ພວກເຮົາ</a></li>
+            </ul>
+        </nav>
+        <!-- .nav-menu -->
+    </div>
+</header>
+<!-- End Header -->
